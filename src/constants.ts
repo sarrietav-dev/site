946a31715810143ac940bfe032f71f784a35a586
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2025-07-09 18:05:19 -0500

    Update social media links in constants.ts to reflect new user profiles

--- src/constants.ts
+++ src/constants.ts
@@ -20,25 +20,25 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/sarrietav-dev",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/sarrietav",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/sarrietav/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:sarrietav+site@protonmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
