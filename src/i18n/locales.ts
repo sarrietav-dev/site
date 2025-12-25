b16f88d22652b500f8831c558b1d76211345fa93
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2025-12-25 13:58:08 -0500

    remove everything

--- src/i18n/locales.ts
+++
@@ -1,9 +0,0 @@
-export const LOCALES = {
-  en: "en",
-  es: "es",
-} as const;
-
-export const DEFAULT_LOCALE = "en";
-export const SUPPORTED_LOCALES = Object.keys(LOCALES) as Locale[];
-
-export type Locale = (typeof LOCALES)[keyof typeof LOCALES];
