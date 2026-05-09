fa9283d73262680b4f0feaf914985be7a30df759
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2026-05-09 13:22:25 -0500

    add RSS feed at /rss.xml

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Sebastian Arrieta',
+    description: 'Software engineer writing about technology and ideas.',
+    site: context.site,
+    items: blog
+      .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/blog/${post.id}/`,
+      })),
+    customData: `<language>en-us</language>`,
+  });
+}
